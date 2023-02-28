be92ff526505fc236dc06d0ce8d0a03ac25ef983
“harshitv804” <100853494+harshitv804@users.noreply.github.com>
2023-02-28 10:45:47 +0530

    changes

---
+++ SEM 6/Project 1/salary_data.R
@@ -0,0 +1,27 @@
+count<-table(mtcars$gear)
+View(count)
+plot(count)
+
+barplot(count)
+barplot(count, horiz=TRUE)
+barplot(count, main="Sample bar plot",
+        xlab = "Improvement",
+        ylab = "Frequency",
+        legend = rownames(count),
+        col=c("red","yellow","green"))
+pie(count)
+
+salary<-read.csv("Salary_data.csv")
+View(salary)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(salary,aes(x=Experience,y=Salary))+
+  geom_point()
+
+ggplot(salary,aes(x=Experience,y=Salary))+
+  geom_line()
+
+ggplot(salary,aes(x=Experience,y=Salary))+
+  geom_boxplot()
